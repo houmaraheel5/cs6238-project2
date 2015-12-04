6013c5bdcf82b76029c1cdd2f8e812e4920b3019
Kyle Koza <kyle.koza@gatech.edu>
2015-12-04 15:12:31 -0500

    Copy client.py to client workspace

--- createClient.py
+++ createClient.py
@@ -14,11 +14,19 @@ args = vars(args)
 
 if not os.path.isdir(os.path.join(BASEDIR, args['client'])):
     os.mkdir(os.path.join(BASEDIR, args['client']))
+
+with open('client.py', 'rb') as src:
+    with open(os.path.join(BASEDIR, args['client'], 'client.py'), 'wb') as dest:
+        dest.write(src.read())
+
 os.chdir(os.path.join(BASEDIR, args['client']))
 
 zip_file = StringIO.StringIO()
-r = requests.get(BASEURL + "/register/" + args['client'], verify=False)
-zip_file.write(r.content)
+r = requests.get(BASEURL + "/register/" + args['client'], verify=False, stream=True)
+
+for chunk in r.iter_content(16):
+    zip_file.write(chunk)
+
 archive = zipfile.ZipFile(zip_file, 'r')
 archive.extractall()
 
